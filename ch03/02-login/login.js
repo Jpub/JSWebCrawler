53d8cbd2d20ecf19e7a90e75744a1b141ff2e9d9
Jihwan Hyun <lemonadegt@gmail.com>
2017-09-22 18:23:14 +0900

    ch03/02-login/login.js 수정
    '새 방명록 수' 의 css선택자가 nth-child(5) 가 아닌 4로 바뀌어야 합니다.

--- ch03/02-login/login.js
+++ ch03/02-login/login.js
@@ -29,7 +29,7 @@ casper.then(function(){
 
 casper.then(function(){        
     var getGuestBook = function(){                
-      return document.querySelector("#blogInfo > ul > li:nth-child(5) > span.day").innerText;
+      return document.querySelector("#blogInfo > ul > li:nth-child(4) > span.day").innerText;
     };
     console.log("새 방명록 수 : " + this.evaluate(getGuestBook));
 });
